c3384db82a25dde88bdd6cb06950834171223a7c
abhijith T A <abhijithajith13@gmail.com>
2024-11-15 12:16:04 +0530

    added env to the backend

--- todo-app-frontend/vite.config.js
+++ todo-app-frontend/vite.config.js
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Check if we're in development mode
-const isDevelopment = process.env.NODE_ENV === 'development';
-
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: isDevelopment
-      ? {
-          '/api': {
-            target: 'http://localhost:5000',  
-            changeOrigin: true,
-          },
-        }
-      : undefined,
-  },
-  define: {
-    'process.env.BACKEND_URL': JSON.stringify(isDevelopment ? '/api' : 'https://hatio-take-home-challenge.onrender.com'),
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BACKEND_URL,
+        changeOrigin: true,
+      },
+    },
   },
 });
